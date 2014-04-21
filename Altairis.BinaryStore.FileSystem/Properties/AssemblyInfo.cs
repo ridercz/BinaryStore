3be1763444b340cc9dadab9e54ae83d4268b1524
Michal Altair Valášek <michal.valasek@altairis.cz>
2014-04-21 02:02:11 +0200

    Changed how NuGet packages are generated

--- Altairis.BinaryStore.FileSystem/Properties/AssemblyInfo.cs
+++ Altairis.BinaryStore.FileSystem/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Security;
 [assembly: AssemblyProduct("Altairis Binary Store")]
 [assembly: AssemblyCopyright("Copyright © Altairis, 2012")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
