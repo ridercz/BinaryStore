213090ee8a4bd9285b3e21cb018abc39eb2989d7
Michal Altair Valášek <michal.valasek@altairis.cz>
2014-04-21 02:03:07 +0200

    Updated copyright years

--- Altairis.BinaryStore.FileSystem/Properties/AssemblyInfo.cs
+++ Altairis.BinaryStore.FileSystem/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Security;
 [assembly: AssemblyTitle("Altairis Binary Store - File System Provider")]
 [assembly: AssemblyCompany("Altairis, s. r. o.")]
 [assembly: AssemblyProduct("Altairis Binary Store")]
-[assembly: AssemblyCopyright("Copyright © Altairis, 2012")]
+[assembly: AssemblyCopyright("Copyright © Altairis, 2012-2014")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.1.0")]
--- Samples/ConsoleSample/Properties/AssemblyInfo.cs
+++ Samples/ConsoleSample/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Security;
 [assembly: AssemblyTitle("Altairis Binary Store - Console Application Sample")]
 [assembly: AssemblyCompany("Altairis, s. r. o.")]
 [assembly: AssemblyProduct("Altairis Binary Store")]
-[assembly: AssemblyCopyright("Copyright © Altairis, 2012")]
+[assembly: AssemblyCopyright("Copyright © Altairis, 2012-2014")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.1.0")]
